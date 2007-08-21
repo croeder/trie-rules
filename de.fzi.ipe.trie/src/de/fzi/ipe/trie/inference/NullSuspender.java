a4ff3889bdd0ca50d87325dd2024c24d15761efd
valentin.zacharias <valentin.zacharias@04cb5f01-e12c-0410-a2f5-a39026196eae>
2007-08-21 07:14:40 +0000

    more widgets for the procedural debugger.

--- de.fzi.ipe.trie/src/de/fzi/ipe/trie/inference/NullSuspender.java
+++ de.fzi.ipe.trie/src/de/fzi/ipe/trie/inference/NullSuspender.java
@@ -5,9 +5,9 @@ import de.fzi.ipe.trie.inference.executionTree.ExecutionTreeGoal;
 
 /**
  * A suspender that does not suspend (can be used when no suspending of the inference engine is needed)
- *
+ * This is the default suspender when none is provided. 
  */
-public class NullSuspender implements Suspender{
+public final class NullSuspender implements Suspender{
 
 	public final void performedAction(Action a, ExecutionTreeGoal goal, Rule r) {
 		;
