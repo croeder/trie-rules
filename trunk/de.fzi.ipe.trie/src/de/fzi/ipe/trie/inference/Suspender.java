32680ced99e3f570ebb951f6da9b78b21e48c628
valentin.zacharias <valentin.zacharias@04cb5f01-e12c-0410-a2f5-a39026196eae>
2007-08-21 09:12:37 +0000

    Inference engine now stops at Call, Redo, Exit, Fail (and some others) and the explanation widget can give *some* explanation for each of these.

--- trunk/de.fzi.ipe.trie/src/de/fzi/ipe/trie/inference/Suspender.java
+++ trunk/de.fzi.ipe.trie/src/de/fzi/ipe/trie/inference/Suspender.java
@@ -5,7 +5,7 @@ import de.fzi.ipe.trie.inference.executionTree.ExecutionTreeGoal;
 
 public interface Suspender {
 
-	public enum Action { TryingGoal } ;
+	public enum Action { CALLING_GOAL, RETRY_GOAL, SUCCESS, END, FAIL_GOAL, EXIT_GOAL, ADD_RULE_TO_EXECUTION_TREE} ;
 	
 	/**
 	 * Method that stops the thread if the current state matches the conditions specified in the suspender. Restarts the thred on 
