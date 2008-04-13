ca95207dcff9e40a5d3ef7ca2c1d7580a7076633
valentin.zacharias <valentin.zacharias@04cb5f01-e12c-0410-a2f5-a39026196eae>
2008-04-13 15:40:08 +0000

    Myriad more bugfixes related to the introduction of the DebuggerFile class

--- de.fzi.ipe.trie.fileManagement/src/de/fzi/ipe/trie/filemanagement/gui/FileLabelProvider.java
+++ de.fzi.ipe.trie.fileManagement/src/de/fzi/ipe/trie/filemanagement/gui/FileLabelProvider.java
@@ -10,7 +10,7 @@ import de.fzi.ipe.trie.filemanagement.model.DebuggerFile;
 public class FileLabelProvider extends LabelProvider {
 
 	public String getText(Object element) {
-		if (element instanceof DebuggerFile) return ((DebuggerFile)element).	getName();
+		if (element instanceof DebuggerFile) return ((DebuggerFile)element).getName();
 		else return super.getText(element);
 	}
 	
