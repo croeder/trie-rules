cfc3cda20fd8ef55a2f614489ff9803a63df7b1f
valentin.zacharias <valentin.zacharias@04cb5f01-e12c-0410-a2f5-a39026196eae>
2008-04-13 15:22:01 +0000

    bugfix, labels for DebuggerFiles not shown

--- de.fzi.ipe.trie.fileManagement/src/de/fzi/ipe/trie/filemanagement/gui/FileLabelProvider.java
+++ de.fzi.ipe.trie.fileManagement/src/de/fzi/ipe/trie/filemanagement/gui/FileLabelProvider.java
@@ -3,15 +3,15 @@
  */
 package de.fzi.ipe.trie.filemanagement.gui;
 
-import java.io.File;
-
 import org.eclipse.jface.viewers.LabelProvider;
+
+import de.fzi.ipe.trie.filemanagement.model.DebuggerFile;
 
 public class FileLabelProvider extends LabelProvider {
 
 	public String getText(Object element) {
-		if (element instanceof File) return ((File)element).getName();
+		if (element instanceof DebuggerFile) return ((DebuggerFile)element).	getName();
 		else return super.getText(element);
 	}
-			
+	
 }
