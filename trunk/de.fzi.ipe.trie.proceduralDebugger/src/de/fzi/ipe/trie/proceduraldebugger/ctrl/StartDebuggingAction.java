080dfb1a4092072a53466c9ca85a7fdbe0a7c1dd
valentin.zacharias <valentin.zacharias@04cb5f01-e12c-0410-a2f5-a39026196eae>
2008-04-15 08:28:24 +0000

    Changed the procedural debugger to find the query automatically, removed one button.

--- trunk/de.fzi.ipe.trie.proceduralDebugger/src/de/fzi/ipe/trie/proceduraldebugger/ctrl/StartDebuggingAction.java
+++ trunk/de.fzi.ipe.trie.proceduralDebugger/src/de/fzi/ipe/trie/proceduraldebugger/ctrl/StartDebuggingAction.java
@@ -1,25 +1,43 @@
 package de.fzi.ipe.trie.proceduraldebugger.ctrl;
 
+import org.eclipse.swt.widgets.Shell;
+
+import de.fzi.ipe.trie.Rule;
+import de.fzi.ipe.trie.proceduraldebugger.model.DatamodelAccess;
 import de.fzi.ipe.trie.proceduraldebugger.model.ReasoningAccess;
 
 public class StartDebuggingAction extends ButtonEnabledAction {
 
-	private static StartDebuggingAction instance = new StartDebuggingAction();
-
-	public static StartDebuggingAction getInstance() {
+	private static StartDebuggingAction instance;
+	
+	private Shell shell;
+	
+	
+	public static StartDebuggingAction getInstance(Shell shell) {
+		if (instance == null) {
+			instance = new StartDebuggingAction(shell);
+		}
 		return instance;
 	}
 	
 	
-	public StartDebuggingAction () {
+	private StartDebuggingAction (Shell shell) {
+		this.shell = shell;
 		setText("Start Debugging");
 		setShortText("Start");
 		setToolTipText("Start Debugging");
-		setEnabled(false);
+		setEnabled(true);
 	}
+	
+	
 	
 	@Override
 	public void run() {
+		//try to find a rule named 'Query' 
+		Rule rule = DatamodelAccess.getKnowledgeBase().getRuleBase().getRule("Query");
+		if (rule != null) ReasoningAccess.setStartingPoint(rule);
+		else SelectRuleAction.getInstance(shell).run();
+		
 		ReasoningAccess.startDebugging();
 		StepAction.getInstance().setEnabled(true);
 		StopAction.getInstance().setEnabled(true);
