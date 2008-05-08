ba398b0adbad6c18358e5a054be35432dc2c5c98
valentin.zacharias <valentin.zacharias@04cb5f01-e12c-0410-a2f5-a39026196eae>
2008-05-08 13:21:51 +0000

    more logging

---
+++ trunk/de.fzi.ipe.trie.rcp/src/de/fzi/ipe/trie/rcp/todo.java
@@ -0,0 +1,17 @@
+package de.fzi.ipe.trie.rcp;
+
+// Clean Log outputs of , and \n
+// Stop Procedural Debugger from making assumptions
+// Log Refresh
+// Log Atom Focus 
+// Log Atom activated, deactivated Event
+//TODO Real output
+
+// Procedural Debugger Logging
+	// action logging
+	// logging suspend listener
+	// does it work after changing files?
+
+//TODO Visualization logging
+
+//File Management Logging
